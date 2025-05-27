20cbc66b9010c5a0f8f2b6a6ef65909e71528e16
JIMENEZ <hector-adrian.jimenez-perez@capgemini.com>
2025-05-27 11:11:29 -0600

    r

--- src/main/java/CodeExercises/Tester.java
+++ src/main/java/CodeExercises/Tester.java
@@ -2,8 +2,7 @@ package CodeExercises;
 
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Tester {
@@ -26,13 +25,13 @@ public class Tester {
                 "13. REVERSE A NUMBER / REVERSE A WORD\n",
                 "14. SUMAR TODOS LOS ELEMENTOS DE  UN ARRAY\n",
                 "15. CONTAR LAS VECES QUE SE REPITE UNA PALABRA EN UN TEXTO \n",
-                "16. ORDENAR UN ARRAY ASC O DESC // no creo pregunten porque la implementacion mas facil es de burbuja pero no es eficiente\n",
-                "17. ENCUENTRA LOS NUMEROS REPETIDOS EN DOS ARRAYS\n",
-                "18. DEVUELVE EL NUMERO QUE NO SE REPITE EN UN ARREGLO, LOS DEMAS NUMEROS SE REPITEN UNA VEZ\n",
-                "19. DADO UN ARREGLO DE NUMEROS O PALABRAS, DEVUELVE EL ARREGLO QUITANDO LOS NUMEROS O PALABRAS QUE SE REPITEN\n",
-                "20. ORDENA UNA LISTA DE PALABRAS ",
-                "21. DADO UN ARREGLO, DEVUELVE UNA LISTA O ARREGLO QUE CONTENGA UNICAMENTE LOS NUMEROS REPETIDOS",
-                "22. compare same indexes of 2 different arrays and create a new array for matching values"};
+                "16. ENCUENTRA LOS NUMEROS REPETIDOS EN DOS ARRAYS\n",
+                "17. DEVUELVE EL NUMERO QUE NO SE REPITE EN UN ARREGLO, LOS DEMAS NUMEROS SE REPITEN UNA VEZ\n",
+                "18. DADO UN ARREGLO DE NUMEROS O PALABRAS, DEVUELVE EL ARREGLO QUITANDO LOS NUMEROS O PALABRAS QUE SE REPITEN\n",
+                "19. ORDENA UNA LISTA DE PALABRAS ",
+                "20. DADO UN ARREGLO, DEVUELVE UNA LISTA O ARREGLO QUE CONTENGA UNICAMENTE LOS NUMEROS REPETIDOS",
+                "21. compare same indexes of 2 different arrays and create a new array for matching values",
+                "22. ORDENAR UN ARRAY ASC O DESC // no creo pregunten porque la implementacion mas facil es de burbuja pero no es eficiente\n"};
 
         Random random = new Random();
         int indiceAleatorio = random.nextInt(codeChallenges.length);
@@ -50,31 +49,28 @@ public class Tester {
                 "coconut", "strawberry", "orange"};
         String text = "el arbol tiene frutas, las frutas son buenas, las frutas rojas son mejores, las frutas verdes";
         int[] numbers = {4,3,2,1};
+        int[] numbers2 = {4,6,9,1};
           char[] letters = {'a','a','c','b','b','a'};
-
-        System.out.println(findIndexesOfNumbersThatAddToTarget(numbers, 4)[0]);
-        System.out.println(findIndexesOfNumbersThatAddToTarget(numbers, 4)[1]);
-
+          findRepeatedNumbers(numbers, numbers2);
     }
-    static int [] findIndexesOfNumbersThatAddToTarget(int  [] numbers, int target) {
-        HashMap<Integer, Integer> complementMap = new HashMap<>();
-        for (int i = 0; i < numbers.length ; i++) {
-            int complement = target - numbers[i];
-            if (complementMap.containsKey(complement)){
-                return new int [] {complementMap.get(complement), i};
-            }
-            complementMap.put(numbers[i], i);
+
+    static int findRepeatedNumbers(int [] array1, int [] array2){
+      //encuentra los numeros repetidos en 2 arrays
+        HashSet<Integer> numbersCopy = new HashSet<>();
+
+        for (int number : array1) {
+            numbersCopy.add(number);
         }
-        return new int[]{};
+        System.out.println(numbersCopy);
+        for (int number : array2) {
+            if (numbersCopy.contains(number)){
+                System.out.println("repeated numbers are: " + number);
+            }
+        }
+
+        return 0;
     }
 
-    static long countWithFilter(String word, char target){
-        return word.chars().filter(ch -> ch == target).count();
-    }
-
-    static long countNumberWithFilter(int [] numbers, int target){
-      return Arrays.stream(numbers).filter(n -> n == target).count();
-    }
 }
 
 
